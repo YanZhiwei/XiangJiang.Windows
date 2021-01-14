521bb1553532ae5a14ff27d5e58a9ccd563bda72
言志伟 <Yan.Zhiwei@hotmail.com>
2021-01-14 19:09:52 +0800

    add

---
+++ XiangJiang.WindowsTests/Core/DataProtectionTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XiangJiang.Windows.Core;
+
+namespace XiangJiang.WindowsTests.Core
+{
+    [TestClass]
+    public class DataProtectionTests
+    {
+        private readonly DataProtection _dataProtection;
+
+        public DataProtectionTests()
+        {
+            _dataProtection = new DataProtection(Encoding.Default.GetBytes("aaabbbccddffgghhddaa"), "XiangJiang");
+        }
+
+        [TestMethod]
+        public void ProtectTest()
+        {
+            var protectText = _dataProtection.Protect("hello", DataProtectionScope.LocalMachine);
+            var actual = _dataProtection.Unprotect(protectText, DataProtectionScope.LocalMachine);
+            Assert.IsNotNull(actual, "hello");
+        }
+    }
+}
